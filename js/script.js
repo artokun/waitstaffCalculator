bc03abf4f75387223b396b02fffeaef796fc2698
Artokun <art.longbottom.jr@gmail.com>
2015-02-27 19:14:18 -0800

    Validations are complete
    Eventually I want to make a cash tip vs percent tip

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-angular.module('myApp', [])
+angular.module('myApp', ['ngMessages'])
   .controller('waitstaffCtrl', ['$scope', function ($scope) {
     //persistent earnings totals
     $scope.staff = {
@@ -17,24 +17,27 @@ angular.module('myApp', [])
     };
     //event on submit
     $scope.newMeal = function (input) {
-      var taxRate, tipPercent, tipCash = 0;
-      $scope.customer.basePrice = $scope.input.baseMealPrice;
-      $scope.customer.tax = $scope.input.taxRate;
-      taxRate = $scope.input.taxRate / 100;
-      tipPercent = $scope.input.tipPercent / 100;
-      //tipCash = $scope.input.tipCash;
-      //  if (tipCash == 0) {
-      //     $scope.customer.tipPercent = $scope.customer.basePrice * tipPercent;
-      //  } else {
-      //     $scope.customer.tipCash = tipCash;
-      //  }
-      $scope.customer.tipPercent = $scope.customer.basePrice * tipPercent;
-      $scope.customer.subtotal = (taxRate * $scope.customer.basePrice) + $scope.customer.basePrice;
+      if ($scope.myForm.$submitted && $scope.myForm.$valid) {
+        var taxRate, tipPercent, tipCash = 0;
+        $scope.customer.basePrice = $scope.input.baseMealPrice;
+        $scope.customer.tax = $scope.input.taxRate;
+        taxRate = $scope.input.taxRate / 100;
+        tipPercent = $scope.input.tipPercent / 100;
+        //tipCash = $scope.input.tipCash;
+        //  if (tipCash == 0) {
+        //     $scope.customer.tipPercent = $scope.customer.basePrice * tipPercent;
+        //  } else {
+        //     $scope.customer.tipCash = tipCash;
+        //  }
+        $scope.customer.tipPercent = $scope.customer.basePrice * tipPercent;
+        $scope.customer.subtotal = (taxRate * $scope.customer.basePrice) + $scope.customer.basePrice;
 
-      $scope.staff.tip = $scope.staff.tip + $scope.customer.tipPercent;
-      $scope.staff.mealCount++;
-      $scope.staff.ATPM = $scope.staff.tip / $scope.staff.mealCount;
-      $scope.input = {};
+        $scope.staff.tip = $scope.staff.tip + $scope.customer.tipPercent;
+        $scope.staff.mealCount++;
+        $scope.staff.ATPM = $scope.staff.tip / $scope.staff.mealCount;
+        $scope.input = {};
+        $scope.myForm.$setPristine();
+      };
     };
     //event on Cancel
     $scope.cancel = function () {
@@ -47,15 +50,15 @@ angular.module('myApp', [])
         tipPercent: 0,
         tipCash: 0
       };
+      $scope.myForm.$setPristine();
     };
     //event on Reset
     $scope.reset = function () {
-      $scope.cancel;
       $scope.staff = {
-      tip: 0,
-      mealCount: 0,
-      ATPM: 0
+        tip: 0,
+        mealCount: 0,
+        ATPM: 0
+      };
+      $scope.cancel();
     };
-    };
-}])
-;
+}]);
